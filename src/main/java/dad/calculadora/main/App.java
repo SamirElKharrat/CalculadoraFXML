50bea16c5faf6f023b24ec7f02272a2c67c8d9ad
SamirElKharrat <samirelkharrat@gmail.com>
2020-11-02 12:12:53 +0000

    preparamos la app

--- src/main/java/dad/calculadora/main/App.java
+++ src/main/java/dad/calculadora/main/App.java
@@ -1,16 +1,25 @@
 package dad.calculadora.main;
 
+import dad.calculadora.fxml.CalculadoraController;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
 
+public class App extends Application {
+	
+	private CalculadoraController controller;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		controller = new CalculadoraController();
 		
+		Scene scene = new Scene(controller.getView(), 320, 200);
+		primaryStage.setTitle("Calculadora");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
